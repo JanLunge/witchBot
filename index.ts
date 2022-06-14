9229afd35ad4a64307ec0e91a9089f5c0ab188b9
Jan Lunge <janlunge1993@gmail.com>
2022-06-14 19:25:37 +0200

    added simple commands

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log("hello world4")
+require('dotenv').config()
+// console.log("hello world4", process.env.TWITCH_OAUTH_TOKEN)
+console.log("Starting witchBot")
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+    options: { debug: true },
+    connection: {
+        secure: true,
+        reconnect: true
+    },
+    identity: {
+        username: 'janlunge',
+        password: process.env.TWITCH_OAUTH_TOKEN
+    },
+    channels: ['janlunge']
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    // Ignore echoed messages.
+    if(self) return;
+    // TODO: display chat
+
+    // match to commands
+    const lowercaseMessage = message.toLowerCase()
+
+    if(/!/.test(lowercaseMessage) === false){
+        console.log("not a command")
+        return
+    }
+
+    if(/!hello/.test(lowercaseMessage)) {
+        client.say(channel, `@${tags.username}, Yo what's up`);
+    }
+});
